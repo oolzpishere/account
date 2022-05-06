620c7b9af40a7255ac64150d2200fd1a3334be16
Ernest Lee <oolzpishere@126.com>
2022-05-06 10:55:31 +0800

    move initializers/omniauth.rb out

--- lib/template/omniauth.rb
+++ lib/template/omniauth.rb
@@ -1,16 +1,9 @@
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :wechat, ENV["WECHAT_TEST_APP_ID"], ENV["WECHAT_TEST_APP_SECRET"],
-   :client_options => {
-        site:          "https://api.weixin.qq.com",
-        # authorize_url: "https://open.weixin.qq.com/connect/oauth2/authorize?#wechat_redirect",
-        # 返回code与state，OmniAuth use code to get wechat info and redirect to /auth/wechat/callback
-        authorize_url: "https://www.sflx.com.cn/get-weixin-code.html?#wechat_redirect",
-        token_url:     "/sns/oauth2/access_token",
-        token_method:  :get
-      },
-   :authorize_params => {:scope => "snsapi_userinfo"}
-  # test account
-  # provider :wechat, ENV["WECHAT_TEST_APP_ID"], ENV["WECHAT_TEST_APP_SECRET"], :authorize_params => {:scope => "snsapi_base"}
-
+      :authorize_params => {:scope => "snsapi_userinfo"},
+      :client_options => {authorize_url: "https://sflx.com.cn/get-weixin-code.html"}
 end
+# https://github.com/omniauth/omniauth/issues/1025
+# https://github.com/omniauth/omniauth/wiki/Upgrading-to-2.0
+# For bug: (wechat) Authentication failure! authenticity_error: OmniAuth::AuthenticityError, ForbiddenAccount
+OmniAuth.config.request_validation_phase = Account::TokenVerifier.new
