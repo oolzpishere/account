58e40c4e982d0f7695a99af6399fa5c763e23cd5
Ernest Lee <oolzpishere@126.com>
2022-05-09 12:52:39 +0800

    fix open_wechat.js redirect_url bug

--- app/assets/javascripts/account/open_wechat.js
+++ app/assets/javascripts/account/open_wechat.js
@@ -8,7 +8,7 @@ ready(() => {
   let login_container_id = 'login_container';
   var login_container = document.getElementById(login_container_id);
   var state = login_container.getAttribute('data-state');
-
+  var redirect_url = "https://www.sflx.com.cn/get-weixin-code.html?redirect_uri=" + window.location.protocol + "//" + window.location.host + "/auth/open_wechat/redirect"
   console.log(state);
 
   var obj = new WxLogin({
@@ -16,7 +16,7 @@ ready(() => {
     id: login_container_id,
     appid: "wx4368756ad44ab8c0",
     scope: "snsapi_login",
-    redirect_uri: "https://www.sflx.com.cn/get-weixin-code.html?redirect_uri=http://localhost:3000/auth/open_wechat/redirect",
+    redirect_uri: redirect_url,
     state: state
     // style: "",
     // href: ""
